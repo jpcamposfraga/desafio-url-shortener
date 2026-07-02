75f067ef4c626b1aa72810b68fffa4cd9742598b
jpcamposfraga <jpdrpenna@gmail.com>
2026-07-02 02:24:24 -0300

    fix: fixed endpoints to redirect to the original link and return the url code correctly appended on the base url

--- src/main/java/com/jpdrpenna/url_shortener/UrlController.java
+++ src/main/java/com/jpdrpenna/url_shortener/UrlController.java
@@ -1,9 +1,12 @@
 package com.jpdrpenna.url_shortener;
 
 import com.jpdrpenna.url_shortener.entity.Url;
+import com.jpdrpenna.url_shortener.entity.dto.ResponseUrlDTO;
 import com.jpdrpenna.url_shortener.entity.dto.ShortenUrlRequest;
+import com.jpdrpenna.url_shortener.entity.dto.UrlDTO;
 import com.jpdrpenna.url_shortener.service.UrlService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +17,17 @@ public class UrlController {
     private final UrlService urlService;
 
     @PostMapping("/shorten-url")
-    public ResponseEntity<String> createShortUrl(@RequestBody String originalUrl){
-        ShortenUrlRequest request = new ShortenUrlRequest(originalUrl);
-        String response = urlService.createAndReturnShortUrl(request);
-
+    public ResponseEntity<ResponseUrlDTO> createShortUrl(@RequestBody ShortenUrlRequest request){
+        String shortUrlCode = urlService.createAndReturnShortUrl(request);
+        ResponseUrlDTO response =  new ResponseUrlDTO(shortUrlCode);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/{shortUrl}")
-    public ResponseEntity<String> getOriginalUrl(@PathVariable String shortUrl){
-        Url foundUrl = urlService.findByShortUrl(shortUrl);
+    @GetMapping("/{shortUrlCode}")
+    public ResponseEntity<String> getOriginalUrl(@PathVariable String shortUrlCode){
+        Url foundUrl = urlService.findByShortUrl(shortUrlCode);
         String originalLink = foundUrl.getOriginalUrl();
 
-        return ResponseEntity.ok(originalLink);
+        return ResponseEntity.status(HttpStatus.FOUND).header("Location", originalLink).build();
     }
 }
