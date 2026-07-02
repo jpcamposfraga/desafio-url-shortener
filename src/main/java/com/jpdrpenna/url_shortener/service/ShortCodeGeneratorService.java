5bca88b8eadf9ef53a7b0a0ec0db2d734ddce6d5
jpcamposfraga <jpdrpenna@gmail.com>
2026-07-02 16:18:34 -0300

    fix: added dependency correctly for repository

--- src/main/java/com/jpdrpenna/url_shortener/service/ShortCodeGeneratorService.java
+++ src/main/java/com/jpdrpenna/url_shortener/service/ShortCodeGeneratorService.java
@@ -1,10 +1,14 @@
 package com.jpdrpenna.url_shortener.service;
 
+import com.jpdrpenna.url_shortener.repository.ShortUrlRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 @Service
+@RequiredArgsConstructor
 public class ShortCodeGeneratorService {
+    private final ShortUrlRepository repository;
     private static final String CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     private static final SecureRandom RANDOM = new SecureRandom();
 
@@ -14,6 +18,9 @@ public class ShortCodeGeneratorService {
         for (int i = 0; i<length; i++){
             int index = RANDOM.nextInt(CHARS.length());
             strBuilder.append(CHARS.charAt(index));
+        }
+        if (repository.existsById(strBuilder.toString())){
+            return generateShortCode(length);
         }
         return strBuilder.toString();
     }
