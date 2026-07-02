2463712a8ee03de342944dd2932daf1db6bd6fe2
jpcamposfraga <jpdrpenna@gmail.com>
2026-07-01 23:12:12 -0300

    initial commit: created project

---
+++ src/test/java/com/jpdrpenna/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpdrpenna.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
