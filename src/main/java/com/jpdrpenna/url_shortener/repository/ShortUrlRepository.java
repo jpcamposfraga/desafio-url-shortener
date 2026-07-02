62b0a01de5be6cf6347a83b43592b1c9d0cd7c6a
jpcamposfraga <jpdrpenna@gmail.com>
2026-07-02 16:19:05 -0300

    feat: created method for database cleanup schedule

--- src/main/java/com/jpdrpenna/url_shortener/repository/ShortUrlRepository.java
+++ src/main/java/com/jpdrpenna/url_shortener/repository/ShortUrlRepository.java
@@ -3,7 +3,10 @@ package com.jpdrpenna.url_shortener.repository;
 import com.jpdrpenna.url_shortener.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ShortUrlRepository extends JpaRepository<Url, String> {
     Url findByShortUrlCode(String shortUrlCode);
+    int deleteByExpiresAtBefore(LocalDateTime now);
 
 }
